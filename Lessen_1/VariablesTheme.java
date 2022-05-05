ab637b049da74896f26295edaebe3a00b76ed3b1
Pavel93G <pashavakulenko24@gmail.com>
2022-05-05 14:54:08 +0700

    Fixed VariablesTheme

--- Lessen_1/VariablesTheme.java
+++ Lessen_1/VariablesTheme.java
@@ -1,8 +1,6 @@
 public class VariablesTheme {
-    public static void main(String[]args){
-        
-        System.out.println("Задача № 1: Создание переменных и вывод их значений на консоль. ");
-
+    public static void main(String[]args) {
+        System.out.print("\nЗадача № 1: Создание переменных и вывод их значений на консоль. ");
         byte ram = 16;
         short ssd = 256;
         int chip = 7;
@@ -15,183 +13,134 @@ public class VariablesTheme {
 
         boolean quiet = true;
 
-        System.out.println(" В качестве информации была использована информация о моём ноутбуке."); 
-        System.out.println("Оперативная память: " + ram + " гб.");
-        System.out.println("Объём жёсткого диска : " + ssd + " гб.");
-        System.out.println("Чип: " + "Apple M1, " + chip + " ‑ ядерный графический процессор.");
-        System.out.println("Срок службы батареи: До " + batteryLife + " До часов работы в интернете по беспроводной сети.");
-        System.out.println("Дисплей: Диагональ " + display + " Дюйма.");
-        System.out.println("Все: " + weight + " кг.");
-        System.out.println("Производитель : " + manufacturer + "pple.");
-        System.out.println("Тихий: " + quiet);
-        System.out.println("");
+        System.out.print("\nВ качестве информации была использована информация о моём ноутбуке."); 
+        System.out.print("\nОперативная память: " + ram + " гб.");
+        System.out.print("\nОбъём жёсткого диска : " + ssd + " гб.");
+        System.out.print("\nЧип: " + "Apple M1, " + chip + " ‑ ядерный графический процессор.");
+        System.out.print("\nСрок службы батареи: До " + batteryLife + " До часов работы в интернете по беспроводной сети.");
+        System.out.print("\nДисплей: Диагональ " + display + " Дюйма.");
+        System.out.print("\nВсе: " + weight + " кг.");
+        System.out.print("\nПроизводитель : " + manufacturer + "pple.");
+        System.out.print("\nТихий: " + quiet);
+        System.out.print("\n");
        
-        System.out.println("Задача №2 : Расчет стоимости товара со скидкой.");
-        int productCost1 = 100; 
-        int productCost2 = 200;
+        System.out.print("\nЗадача №2 : Расчет стоимости товара со скидкой.");
+        int productCostX = 100; 
+        int productCostY = 200;
         int productDiscount = 11;
-        
-        double discountAmount = (productCost1 + productCost2 * productDiscount) / 100;
 
-        double totalCost = (productCost1 + productCost2 - discountAmount);
- 
-        System.out.println("Сумма скидки: " + productDiscount + " %.");
-        System.out.println("Общая стоимость товара со скидка: " + totalCost + " рублей.");
-        System.out.println("");
+        double discountAmount = (productCostX + productCostY * productDiscount) / 100;
+        double totalCost = (productCostX + productCostY - discountAmount);
 
-        System.out.println("Задача №3: Вывод на консоль слова JAVA: ");
-        System.out.println("   J    a  v     v  a");
-        System.out.println("   J   a a  v   v  a a");
-        System.out.println("J  J  aaaaa  V V  aaaaa");
-        System.out.println(" JJ  a     a  V  a     a");
-        System.out.println("");
+        System.out.print("\nСумма скидки: " + productDiscount + " %.");
+        System.out.print("\nОбщая стоимость товара со скидка: " + totalCost + " рублей.");
+        System.out.print("\n");
 
-        System.out.println("Задача №4: Отображение min и max значений числовых типов данных: ");
+        System.out.print("\nЗадача №3: Вывод на консоль слова JAVA: ");
+        System.out.print("\n   J    a  v     v  a");
+        System.out.print("\n   J   a a  v   v  a a");
+        System.out.print("\nJ  J  aaaaa  V V  aaaaa");
+        System.out.print("\n JJ  a     a  V  a     a");
+        System.out.print("\n");
+
+        System.out.print("\nЗадача №4: Отображение min и max значений числовых типов данных: ");
         byte byteMax = 127;
         short shortMax = 32_767;
         int intMax = 2147483647;
         long longMax = 9223372036854775807L;
 
-        System.out.println("byteMax: " + byteMax);
-        System.out.println("shortMax: " + shortMax);
-        System.out.println("intMax: " + intMax);
-        System.out.println("longMax: " + longMax);
-        System.out.println("");
+        System.out.print("\nbyteMax: " + byteMax);
+        System.out.print("\nshortMax: " + shortMax);
+        System.out.print("\nintMax: " + intMax);
+        System.out.print("\nlongMax: " + longMax);
+        System.out.print("\n");
 
-        ++byteMax;
-        ++shortMax;
-        ++intMax;
-        ++longMax;
+        System.out.print("\nbyteMin: " + ++byteMax);
+        System.out.print("\nshortMin: " + ++shortMax);
+        System.out.print("\nintMin: " + ++intMax);
+        System.out.print("\nlongMin: " + ++longMax);
+        System.out.print("\n");
 
-        System.out.println("byteMin: " + byteMax);
-        System.out.println("shortMin: " + shortMax);
-        System.out.println("intMin: " + intMax);
-        System.out.println("longMin: " + longMax);
-        System.out.println("");
+        System.out.print("\nbyteMax: " + --byteMax);
+        System.out.print("\nshortMax: " + --shortMax);
+        System.out.print("\nintMax: " + --intMax);
+        System.out.print("\nlongMax: " + --longMax);
+        System.out.print("\n");
 
-        --byteMax;
-        --shortMax;
-        --intMax;
-        --longMax;
-
-        System.out.println("byteMax: " + byteMax);
-        System.out.println("shortMax: " + shortMax);
-        System.out.println("intMax: " + intMax);
-        System.out.println("longMax: " + longMax);
-        System.out.println("");
-
-        System.out.println("Задача№5: Перестановка значений переменных: ");
+        System.out.print("\nЗадача№5: Перестановка значений переменных: ");
 
         double numberOne = 297.30;
         double numberTwo = 30.40;
 
-        System.out.println("Число один: " + numberOne);
-        System.out.println("Число два: " + numberTwo);
-        System.out.println("");
+        System.out.print("\nЧисло один: " + numberOne);
+        System.out.print("\nЧисло два: " + numberTwo);
+        System.out.print("\n");
 
-        System.out.println("Перестановка: ");
-        System.out.println("");
-
+        System.out.print("\nПерестановка: ");
         double temp = numberOne;
         numberOne = numberTwo;
         numberTwo = temp;
 
-        System.out.println("Число один: " + numberOne);
-        System.out.println("Число два: " + numberTwo);
-        System.out.println("");
+        System.out.print("\nЧисло один: " + numberOne);
+        System.out.print("\nЧисло два: " + numberTwo);
+        System.out.print("\n");
 
-        System.out.println("Задача№ 6: Вывод символов и их кодов: ");
+        System.out.print("\nЗадача№ 6: Вывод символов и их кодов: ");
         char codeChar1 = 35;
         char codeChar2 = 38;
         char codeChar3 = 64;
         char codeChar4 = 94;
         char codeChar5 = 95;
 
-        System.out.println("codeChar1: 35 " + codeChar1);
-        System.out.println("codeChar2: 38 " + codeChar2);
-        System.out.println("codeChar3: 64 " + codeChar3);
-        System.out.println("codeChar4: 94 " + codeChar4);
-        System.out.println("codeChar5: 95 " + codeChar5);
-        System.out.println("");
+        System.out.print("\n" + codeChar1);
+        System.out.print("\n" + codeChar2);
+        System.out.print("\n" + codeChar3);
+        System.out.print("\n" + codeChar4);
+        System.out.print("\n" + codeChar5);
+        System.out.print("\n");
 
 
-        System.out.println("Задание № 7: Произведение и сумма цифр числа 345");
-        int number = 345;
-        int units = number % 10;
-        int tens = (number / 10) % 10;
-        int hundreds = (number / 10) /10;
-        int multiplication = hundreds * tens * units;
-        System.out.println("Произведение цифр числа 345: " + multiplication);
-        System.out.println("");
-        int sum = hundreds + tens + units;
-        System.out.println("Сумма цифр числа 345: " + sum);
-        System.out.println("");
+        System.out.print("\nЗадание № 7: Произведение и сумма цифр числа 345");
+        int srcNumber = 345;
+        int units = srcNumber % 10;
+        int tens = (srcNumber / 10) % 10;
+        int hundreds = srcNumber / 100;
+        int productDigits = hundreds * tens * units;
+        System.out.print("\nПроизведение цифр числа 345: " + productDigits);
+        System.out.print("\n");
+        int sumDigits = hundreds + tens + units;
+        System.out.print("\nСумма цифр числа 345: " + sumDigits);
+        System.out.print("\n");
 
-        System.out.println("Задание№ 7: Вывод на консоль ASCII-арт Дюка: ");
-        char duke = 32;
-        char duke1 = 47;
-        char duke2 = 92;
-        char duke3 = 95;
-        char duke4 = 40;
-        char duke5 = 41;
+        System.out.print("\nЗадание№ 8: Вывод на консоль ASCII-арт Дюка: ");
+        char space = ' ';
+        char slash = '/';
+        char backslash = '\\';
+        char underlining = '_';
+        char openBracket = '(';
+        char closeBracket = ')';
 
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke1);
-        System.out.println(duke2);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke1);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.println(duke2);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke1);
-        System.out.print(duke3);
-        System.out.print(duke4);
-        System.out.print(duke);
-        System.out.print(duke5);
-        System.out.println(duke2);
-        System.out.print(duke);
-        System.out.print(duke1);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.print(duke);
-        System.out.println(duke2);
-        System.out.print(duke1);
-        System.out.print(duke3);
-        System.out.print(duke3);
-        System.out.print(duke3);
-        System.out.print(duke3);
-        System.out.print(duke1);
-        System.out.print(duke2);
-        System.out.print(duke3);
-        System.out.print(duke3);
-        System.out.println(duke2);
-        System.out.println("");
+        System.out.print("\n" + space + space + space + space +slash + backslash + "\n");
+        System.out.print("" + space + space + space + slash + space + space + backslash + "\n");
+        System.out.print("" + space + space + slash + underlining + openBracket + space + closeBracket + backslash + "\n");
+        System.out.print("" + space + slash + space + space + space + space + space + space + backslash + "\n");
+        System.out.print("" + slash + underlining + underlining + underlining + underlining + slash + backslash + underlining + underlining + backslash + "\n");
+        System.out.print("\n");
 
-        System.out.println("Задача №8: Отображение количества сотен, десятков и единиц числа 128");
-        int num = 123;
-        System.out.println("Единицы: " + num % 10);
-        System.out.println("Десятков: " + num / 10 % 10);
-        System.out.println("Сотен: " + num / 100);
-        System.out.println("");
+        System.out.print("\nЗадача №9: Отображение количества сотен, десятков и единиц числа.");
+        int srcNum = 123;
+        System.out.print("\nЕдиницы: " + srcNum % 10);
+        System.out.print("\nДесятков: " + srcNum / 10 % 10);
+        System.out.print("\nСотен: " + srcNum / 100);
+        System.out.print("\n");
 
-        System.out.println("Задача №9: Преобразование секунд");
-        System.out.println("");
-        int numberOfSeconds = 86399;
-        int hours = numberOfSeconds / 3600;
+        System.out.print("\nЗадача №10: Преобразование секунд");
+        int numbersOfSeconds = 86399;
+        int hours = numbersOfSeconds / 3600;
         int minutes = hours / 60;
         int seconds = minutes / 60;
 
-        System.out.println(hours + " : " + minutes + " : " + seconds);
-        System.out.println("");
+        System.out.print("\n" + hours + " : " + minutes + " : " + seconds);
+        System.out.print("\n");
             }
         }
