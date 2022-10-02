5ece65dfd45fe23b12510e26674e51b7208d19e8
Pavel93G <pashavakulenko24@gmail.com>
2022-10-02 23:15:26 +0700

    Lessen2_HW_Calculator

---
+++ Lessen_2/Calculator.java
@@ -0,0 +1,51 @@
+public class Calculator {
+
+    private int firstNumber;
+    private char mathOperation;
+    private int secondNumber;
+
+    public void calculate() {
+        switch(mathOperation) {
+        case '+':
+            System.out.println(firstNumber + secondNumber);
+            break;
+        case '-':
+            System.out.println(firstNumber - secondNumber);
+            break;
+        case '*':
+            System.out.println(firstNumber * secondNumber);
+            break;
+        case '/':
+            System.out.println(firstNumber / secondNumber);
+            break;
+        case '^':
+            pow();
+            break;
+        case '%':
+            System.out.println(firstNumber % secondNumber);
+            break;
+        default:
+            System.out.println("Ошибка! Ввели не верный знак.");
+        }
+    }
+
+    public void pow() {
+    int result = 1;
+    for(int i = 0; i < secondNumber; i++) {
+        result = result * firstNumber;
+    }
+    System.out.println(result);
+}
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setMathOperation(char mathOperation) {
+        this.mathOperation = mathOperation;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+}
