b1b7ba5f428f058129413465785220d49cd3b587
PavelGalkin <98624359+Pavel93G@users.noreply.github.com>
2022-10-23 01:56:09 +0700

    Delete Lessen_1 directory

--- Lessen_1/MyFirstApp.java
+++
@@ -1,1 +0,0 @@
-import java.util.Scanner;public class MyFirstApp {    public static void main(String[] args){        System.out.println("Написанно однажды, работает везде");        Scanner console = new Scanner(System.in);        System.out.println("Введите своё имя: ");        System.out.println(console.nextLine());    }}
